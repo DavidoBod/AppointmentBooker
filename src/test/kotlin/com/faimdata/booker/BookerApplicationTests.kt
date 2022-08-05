e7b59fc2505cd209d09a096d5963b96d6cb8d40b
David Bod <davidbodromian@outlook.com>
2022-08-04 20:58:49 -0400

    main

---
+++ src/test/kotlin/com/faimdata/booker/BookerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.faimdata.booker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
