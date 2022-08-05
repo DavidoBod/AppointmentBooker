50b9ac1a51d95d6347972ee70330cc1dc9ac36fc
David Bod <davidbodromian@outlook.com>
2022-08-04 20:59:52 -0400

    Appointment and Availability requests and queries

---
+++ src/main/kotlin/com/faimdata/booker/controllers/AvailabilityController.kt
@@ -0,0 +1,35 @@
+package com.faimdata.booker.controllers
+
+import com.faimdata.booker.models.Availability
+import com.faimdata.booker.repositories.AvailabilityRepo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/availabilities")
+class AvailabilityController(
+        @Autowired private val availabilityRepo: AvailabilityRepo
+) {
+
+    @GetMapping("/")
+    fun getAvailabilities(): List<Availability> {
+        return availabilityRepo.findAll()
+    }
+
+    @PostMapping("/save")
+    fun saveAvailability(@RequestBody availability: Availability): String {
+        availabilityRepo.save(availability)
+        return "Saved availability..."
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteAvailability(@PathVariable id: Long): String {
+        availabilityRepo.deleteById(id)
+        return "Deleting availability..."
+    }
+
+    @GetMapping("/provider/{providerId}")
+    fun getProviderAvailabilities(@PathVariable providerId: String): List<Availability> {
+        return availabilityRepo.getProviderAvailabilities(providerId)
+    }
+}
---
+++ src/main/kotlin/com/faimdata/booker/repositories/AppointmentRepo.kt
@@ -0,0 +1,7 @@
+package com.faimdata.booker.repositories
+
+import com.faimdata.booker.models.Appointment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AppointmentRepo : JpaRepository<Appointment, Long> {
+}
