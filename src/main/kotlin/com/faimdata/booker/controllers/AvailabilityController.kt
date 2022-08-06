a572354523bb9e434017ad4efc2dafc1958e7d0c
David Bod <davidbodromian@outlook.com>
2022-08-05 21:03:07 -0400

    cannot insert availabilties at times when providers have appointments

--- src/main/kotlin/com/faimdata/booker/controllers/AvailabilityController.kt
+++ src/main/kotlin/com/faimdata/booker/controllers/AvailabilityController.kt
@@ -2,21 +2,31 @@ package com.faimdata.booker.controllers
 
 import com.faimdata.booker.models.Availability
 import com.faimdata.booker.repositories.AvailabilityRepo
+import com.faimdata.booker.repositories.ProviderRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/availabilities")
 class AvailabilityController(
-        @Autowired private val availabilityRepo: AvailabilityRepo
+        @Autowired private val availabilityRepo: AvailabilityRepo,
+        @Autowired private val providerRepo: ProviderRepo
 ) {
     @GetMapping("/")
     fun getAvailabilities(): List<Availability> {
         return availabilityRepo.findAll()
     }
 
+    @GetMapping("/{id}")
+    fun getAvailability(@PathVariable id: Long): Availability {
+        return availabilityRepo.findById(id).get()
+    }
+
     @PostMapping("/save")
     fun saveAvailability(@RequestBody availability: Availability): String {
+        if (providerRepo.containsAppointment(availability.provider.id, availability.start)) {
+            return "Provider is unavailable at this time..."
+        }
         availabilityRepo.save(availability)
         return "Saved availability..."
     }
--- src/main/kotlin/com/faimdata/booker/repositories/ProviderRepo.kt
+++ src/main/kotlin/com/faimdata/booker/repositories/ProviderRepo.kt
@@ -2,5 +2,11 @@ package com.faimdata.booker.repositories
 
 import com.faimdata.booker.models.Provider
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.sql.Timestamp
 
-interface ProviderRepo : JpaRepository<Provider, String>
+interface ProviderRepo : JpaRepository<Provider, String> {
+    @Query("select count(*) > 0 from Appointment where provider_id = :provider and start = :start")
+    fun containsAppointment(@Param("provider") providerId: String, @Param("start") start: Timestamp): Boolean
+}
