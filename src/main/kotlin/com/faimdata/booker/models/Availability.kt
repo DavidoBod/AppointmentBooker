31d73df9c27fd7005c8268b2089ba4a70c73deef
David Bod <davidbodromian@outlook.com>
2022-08-05 20:39:41 -0400

    no duplicate availabilities

--- src/main/kotlin/com/faimdata/booker/models/Availability.kt
+++ src/main/kotlin/com/faimdata/booker/models/Availability.kt
@@ -2,6 +2,11 @@ package com.faimdata.booker.models
 
 import java.sql.Timestamp
 import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
 
 @Entity
+@Table(uniqueConstraints = [
+    UniqueConstraint(columnNames = ["start", "end", "provider_id"])
+])
 class Availability(id: Long, start: Timestamp, end: Timestamp, provider: Provider) : Session(id, start, end, provider)
